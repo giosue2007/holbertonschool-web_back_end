e48dff065528de7bc1fc8d6df5e2763748be3e69
giosue2007 <toxicgiosue@gmail>
2026-06-17 01:27:16 +0200

    add perm and c

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> list:
     """Run wait_random n times concurrently and return sorted delays."""
     results = []
-    for coro in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    for coro in asyncio.as_completed(coroutines):
         delay = await coro
         results.append(delay)
     return results
