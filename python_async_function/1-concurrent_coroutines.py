6d844a6e93315927ddd9caaad3f8c775731bd97a
giosue2007 <toxicgiosue@gmail>
2026-06-17 01:19:44 +0200

    add perm and c

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for concurrent coroutines."""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """Run wait_random n times concurrently and return sorted delays."""
+    results = []
+    for coro in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
+        delay = await coro
+        results.append(delay)
+    return results
