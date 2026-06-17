dbeb9055788a8348bbffe96a574b3b56a31415f5
giosue2007 <toxicgiosue@gmail>
2026-06-17 14:06:14 +0200

    add perm and c

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 """
-Tasks module.
+Module pour la tâche 4.
 """
 import asyncio
 from typing import List
 
-# Import task_wait_random from the previous task file
+# Import de task_wait_random depuis le fichier précédent
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns task_wait_random n times with the specified max_delay.
-    
-    Returns the list of all the delays (float values) in ascending order
-    without using sort() because of concurrency.
+    Spawns task_wait_random n fois avec le max_delay spécifié.
+
+    Retourne la liste des délais (floats) par ordre croissant
+    grâce à asyncio.as_completed.
     """
-    # Create a list of asyncio.Task objects by calling task_wait_random
+    # Création de la liste de Tasks
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    
+
     delays = []
-    # Use asyncio.as_completed to yield tasks as they finish
+    # asyncio.as_completed récupère les tâches au fur et à mesure de leur réussite
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
-        
+
     return delays
