befd73383fcc38cb03122b22dbc1d334598082dc
giosue2007 <toxicgiosue@gmail>
2026-06-17 14:42:19 +0200

    add perm and c

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 """
-Module pour la tâche 2 : Temps d'exécution de 4 compréhensions.
+Module for task 2.
 """
 import asyncio
 import time
 
-# Import de async_comprehension depuis le fichier précédent
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    Exécute async_comprehension 4 fois en parallèle avec asyncio.gather
-    et mesure le temps d'exécution total.
+    Run async_comprehension 4 times in parallel using asyncio.gather
+    and measure the total runtime.
     """
     start_time = time.perf_counter()
 
-    # Exécution des 4 tâches en parallèle
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
