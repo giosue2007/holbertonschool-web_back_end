dfb9a6e48d82b8127f13ab6e0e6f45585d8b41c8
giosue2007 <toxicgiosue@gmail>
2026-06-17 13:42:05 +0200

    add perm and c

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for measuring runtime."""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure total execution time for wait_n and return total_time / n."""
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start
+    return total_time / n
